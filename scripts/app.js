a5b5ac212278ba99b8778fb36e4dbc80ce700bb0
Kevin-1234 <zengqingyuan@live.com>
2021-11-23 19:36:36 +0930

    2nd commit

--- scripts/app.js
+++ scripts/app.js
@@ -13,7 +13,7 @@
 
 if('serviceWorker' in navigator){
 
-  navigator.serviceWorker.register('/IT-Code-Fair-2020/sw.js')
+  navigator.serviceWorker.register('/IT_code_fair_2021_test/sw.js')
     .then((reg) => console.log('service worker registered!', reg))
     .catch((err) => console.log('service worker failed to register!', err));  
 }
